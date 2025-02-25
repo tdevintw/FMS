f71e1daae100c3863390f4b60de87e858033fdb8
Yasser <aitelghariyasser@gmail.com>
2025-02-25 11:00:07 +0000

    edit global exception handelr to hande body fields

--- src/main/java/dev/yassiraitelghari/fms/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/yassiraitelghari/fms/exception/GlobalExceptionHandler.java
@@ -2,8 +2,13 @@ package dev.yassiraitelghari.fms.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -52,4 +57,16 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
 
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException e){
+        Map<String , String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error->{
+            String fieldName = ((FieldError) error).getField() ;
+            String message = error.getDefaultMessage() ;
+            errors.put(fieldName , message);
+        });
+        return ResponseEntity.badRequest().body(errors) ;
+    }
+
 }
